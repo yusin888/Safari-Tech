84a32973cebe423b12e97efe0cedc64de924bb11
Yusin <yunisaden3@gmail.com>
2024-07-25 18:36:58 +0300

    Add the stripe payment link for dev mode

---
+++ constant.ts
@@ -0,0 +1,9 @@
+export const constants  = {
+    paymentlink:{
+        TechMentorFund:
+            process.env.NODE_ENV === 'development' 
+            ? "https://buy.stripe.com/test_6oE187gX7f45drOeUU"
+            : "https://buy.stripe.com/test_6oE187gX7f45drOeUU"
+        
+    }
+}
